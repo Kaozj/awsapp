8ac6e6e9ddc54f16abe70b8058eefe6b942166c1
zijian <zijiankao@gmail.com>
2024-01-31 14:08:50 +0800

    added storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -27,7 +27,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://plcvbobatrbnlpmicikcsqdgra.appsync-api.ap-southeast-2.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-sce7e3zeibbbnaqhxiktaul3ka"
+    "aws_appsync_apiKey": "da2-sce7e3zeibbbnaqhxiktaul3ka",
+    "aws_user_files_s3_bucket": "imagestoragebucket160744-staging",
+    "aws_user_files_s3_bucket_region": "ap-southeast-2"
 };
 
 
